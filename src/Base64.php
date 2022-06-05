a0f2162f9e9470297801cf8ccec79024eac90b08
JonasAntonio <jayv.jonas@gmail.com>
2022-06-05 17:55:35 -0300

    init

---
+++ src/Base64.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Basic\Jwt;
+
+use DomainException;
+
+class Base64
+{
+    /**
+     * Encodes base64 URL
+     *
+     * @param string $data
+     * @return string
+     */
+    public function base64UrlEncode(string $data): string
+    {
+        return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+    }
+
+    /**
+     * Decodes base64 URL
+     *
+     * @param string $data
+     * @return string
+     */
+    public function base64UrlDecode(string $data): string
+    {
+        $decoded = base64_decode(
+            strtr($data, '-_', '+/') 
+            . str_repeat('=', 3 - (3 + strlen($data)) % 4)
+        );
+
+        if ($decoded === false) {
+            throw new DomainException("Could not decode base64");
+        }
+
+        return $decoded;
+    }
+}
---
+++ src/Jwt.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Basic\Jwt;
+
+class Jwt
+{
+}
